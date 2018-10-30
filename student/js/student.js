96e347d1dd800b4a640ca6f6f0dc31b28056286b
Vishal Naidu <slipfold9@gmail.com>
2018-10-31 01:06:52 +0530

    Update student.js

--- student/js/student.js
+++ student/js/student.js
@@ -1,3 +1,24 @@
+
+xhr = new XMLHttpRequest();
+function check(){
+	xhr.onreadystatechange = userUpdate;
+	xhr.open("POST", "index.php", true);
+	xhr.send();
+}
+function userUpdate(){
+	if(this.readyState == 4 && this.status == 200){
+		var res = this.responseText;
+		b = res.split(';')
+		console.log(b);
+		document.getElementById("name").innerHTML = "Name:"+b[0];
+		document.getElementById("usn").innerHTML = "USN:"+b[1];
+	}
+}
+if ('addEventListener' in window) {
+	window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
+		document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+}
+	
 function events()
 {
 	window.location="../../notifications/php/notify.php";
