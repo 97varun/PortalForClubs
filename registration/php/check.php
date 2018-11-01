78aba637cfc5d0adc5ad399be2347aac846ed74a
Vishal Naidu <slipfold9@gmail.com>
2018-11-01 15:56:49 +0530

    Create check.php

---
+++ registration/php/check.php
@@ -0,0 +1,50 @@
+<?php
+	session_start();
+	$mysqli = new mysqli("localhost", "root", "", "Portal");
+	extract($_POST);
+	if(isset($_POST['name'])){
+		$sql = "SELECT count(1) FROM students WHERE Student_name = '" . $_POST['name'] . "'";
+		$stmt = $mysqli->prepare($sql);
+		$stmt->execute();
+		$stmt->bind_result($found);
+		$stmt->fetch();
+		if($found){
+			echo '-1';
+		}
+		else{
+			echo '0';
+		}
+	}
+	else if(isset($_POST['usn'])){
+		$sql = "SELECT count(1) FROM students WHERE Student_usn = '" . $_POST['usn'] . "'";
+		$stmt = $mysqli->prepare($sql);
+		$stmt->execute();
+		$stmt->bind_result($found);
+		$stmt->fetch();
+		if($found){
+			echo '-1';
+		}
+		else{
+			echo '0';
+		}
+	}
+	
+	else if(isset($_POST['email'])){
+		$sql = "SELECT count(1) FROM students WHERE Student_email = '" . $_POST['email'] . "'";
+		$stmt = $mysqli->prepare($sql);
+		$stmt->execute();
+		$stmt->bind_result($found);
+		$stmt->fetch();
+		if($found){
+			echo '-1';
+		}
+		else{
+			echo '0';
+		}
+	}
+
+
+
+	
+
+?>
