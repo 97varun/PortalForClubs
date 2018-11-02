20527a0850ce2138e0d6e56dc4e4c80090d1b459
sravs05 <33353995+sravs05@users.noreply.github.com>
2018-11-03 00:18:48 +0530

    Update event.php

--- events/php/event.php
+++ events/php/event.php
@@ -14,7 +14,7 @@ $link = mysqli_connect($servername, $username1, $password1, $dbname);
 if (!$link) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$a="SELECT event_name from create_event WHERE event_name='$plan'";
+$a="SELECT event_name from create_event WHERE event_name='$event_name'";
 $result=mysqli_query($link,$a);
 $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
 if(mysqli_num_rows($result)==1)
@@ -29,7 +29,7 @@ print '<script>alert("Event already created!! ");</script>';
 
 	if($bool==true) // checks if bool is true
 	{
-		$sql="INSERT INTO create_event (date,time,end_time,event_name,place,more_info,invite) VALUES ('$date','$time','$end_time','$plan','$where','$detail','$invite')"; //Inserts the value to table registration
+		$sql="INSERT INTO create_event (club_id,date,time,end_time,event_name,place,more_info,invite) VALUES ('$club_id','$date','$time','$end_time','$event_name','$place','$more_info','$invite')"; //Inserts the value to table registration
 echo "<script>setTimeout(\"location.href = '../html/event.html';\",500);</script>";
 print '<script>alert("Successfully Registered! ");</script>';
 	if (mysqli_query($link, $sql) ) {
