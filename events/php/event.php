1ef54da1c63b1783a4a7f78d7ab9cbd5fe84048d
sravs05 <33353995+sravs05@users.noreply.github.com>
2018-11-03 18:15:01 +0530

    Update event.php

--- events/php/event.php
+++ events/php/event.php
@@ -1,43 +1,42 @@
 
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-	extract($_POST);
-    $bool = true;
-
-	$servername = "localhost";
-$username1 = "root";
-$password1 = "";
-$dbname = "portal";
-$link = mysqli_connect($servername, $username1, $password1, $dbname);
-// Check connection
-if (!$link) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-$a="SELECT event_name from create_event WHERE event_name='$event_name'";
-$result=mysqli_query($link,$a);
-$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-if(mysqli_num_rows($result)==1)
+if($_SERVER["REQUEST_METHOD"] == "POST")
 {
-	
-$bool=false;
-	echo "Redirecting... Please wait..";
-echo "<script>setTimeout(\"location.href = '../html/event.html';\",500);</script>";
-print '<script>alert("Event already created!! ");</script>';
-
-}
-
+	extract($_POST);
+	$bool = true;
+	$servername = "localhost";
+	$username1 = "root";
+	$password1 = "";
+	$dbname = "portal";
+	$link = mysqli_connect($servername, $username1, $password1, $dbname);
+	// Check connection
+	if (!$link)
+	{
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	$a="SELECT event_name from create_event WHERE event_name='$event_name'";
+	$result=mysqli_query($link,$a);
+	$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+	if(mysqli_num_rows($result)==1)
+	{
+		$bool=false;
+		echo "Redirecting... Please wait..";
+		echo "<script>setTimeout(\"location.href = '../html/event.html';\",500);</script>";
+		print '<script>alert("Event already created!! ");</script>';
+	}
 	if($bool==true) // checks if bool is true
 	{
 		$sql="INSERT INTO create_event (club_id,date,time,end_time,event_name,place,more_info,invite) VALUES ('$club_id','$date','$time','$end_time','$event_name','$place','$more_info','$invite')"; //Inserts the value to table registration
-echo "<script>setTimeout(\"location.href = '../html/event.html';\",500);</script>";
-print '<script>alert("Successfully Registered! ");</script>';
-	if (mysqli_query($link, $sql) ) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($link);
-}
+		echo "<script>setTimeout(\"location.href = '../html/event.html';\",500);</script>";
+		print '<script>alert("Successfully Registered! ");</script>';
+		if (mysqli_query($link, $sql))
+		{
+			echo "New record created successfully";
+		}
+		else 
+		{
+			echo "Error: " . $sql . "<br>" . mysqli_error($link);
+		}
 	}
-
 }
 ?>
