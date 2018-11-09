983a6192e220b8c5dffdbff8621fe4e1a5270b34
sravs05 <33353995+sravs05@users.noreply.github.com>
2018-11-09 21:46:55 +0530

    Update event.php

--- events/php/event.php
+++ events/php/event.php
@@ -2,7 +2,10 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
+
+	session_start();
 	extract($_POST);
+	extract($_SESSION);
 	$bool = true;
 	$servername = "localhost";
 	$username1 = "root";
@@ -14,6 +17,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 	{
 		die("Connection failed: " . mysqli_connect_error());
 	}
+	$sqll="SELECT Club_ID FROM admin where Admin_USN='$srn';";
+	$ret=mysqli_query($link,$sqll);
+	$row=mysqli_fetch_row($ret);
+	$club_id=$row[0];
 	$a="SELECT event_name from create_event WHERE event_name='$event_name'";
 	$result=mysqli_query($link,$a);
 	$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
@@ -21,13 +28,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 	{
 		$bool=false;
 		echo "Redirecting... Please wait..";
-		echo "<script>setTimeout(\"location.href = '../html/event.html';\",500);</script>";
+		echo "<script>setTimeout(\"location.href = '../../admin/html/admin.html';\",500);</script>";
 		print '<script>alert("Event already created!! ");</script>';
 	}
 	if($bool==true) // checks if bool is true
 	{
 		$sql="INSERT INTO create_event (club_id,date_cur,time_cur,end_time,event_name,place,more_info,invite) VALUES ('$club_id','$date_cur','$time_cur','$end_time','$event_name','$place','$more_info','$invite')"; //Inserts the value to table registration
-		echo "<script>setTimeout(\"location.href = '../html/event.html';\",500);</script>";
+		echo "<script>setTimeout(\"location.href = '../../admin/html/admin.html';\",500);</script>";
 		print '<script>alert("Successfully Registered! ");</script>';
 		if (mysqli_query($link, $sql))
 		{
