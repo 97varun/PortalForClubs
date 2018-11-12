432773084a324c21229b3ebfa60ad2db97f54961
Swethaksn <43950407+Swethaksn@users.noreply.github.com>
2018-11-12 17:03:17 +0530

    Update portal.sql

--- tables-creation/portal.sql
+++ tables-creation/portal.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2018 at 02:59 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.4
+-- Generation Time: Nov 12, 2018 at 12:31 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -66,6 +64,37 @@ INSERT INTO `club` (`Club_name`, `Club_ID`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `clubjoinform`
+--
+
+CREATE TABLE `clubjoinform` (
+  `Club_ID` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `clubjoinform`
+--
+
+INSERT INTO `clubjoinform` (`Club_ID`) VALUES
+('1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clubregistration`
+--
+
+CREATE TABLE `clubregistration` (
+  `Club_ID` varchar(20) NOT NULL,
+  `Member_USN` varchar(20) NOT NULL,
+  `Sem` int(11) NOT NULL,
+  `Branch` varchar(5) NOT NULL,
+  `ReasonToJoin` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `club_member`
 --
 
@@ -109,8 +138,10 @@ CREATE TABLE `create_event` (
 --
 
 INSERT INTO `create_event` (`event_id`, `club_id`, `date_cur`, `time_cur`, `end_time`, `event_name`, `place`, `more_info`, `invite`) VALUES
-(19, '2', '2018-12-14', '19:45:00', '23:45:00', 'Halloween', 'Whitefield', 'With costumes', 'All college students'),
-(20, '3', '2018-12-29', '04:56:00', '04:56:00', 'hhhrty', 'rhrt', 'rty', '54t54');
+(20, '3', '2018-12-29', '04:56:00', '04:56:00', 'hhhrty', 'rhrt', 'rty', '54t54'),
+(8, '1', '2018-12-11', '16:35:00', '16:45:00', 'xxxxxxxxx', 'xxxxxxx', 'xx', 'xxxxxxxxxxxx'),
+(10, '2', '2018-12-27', '14:34:00', '14:34:00', 'Drawing', 'Panini', 'sdf', 'dfsd'),
+(19, '2', '2018-12-14', '19:45:00', '23:45:00', 'Halloween', 'Whitefield', 'With costumes', 'All college students');
 
 -- --------------------------------------------------------
 
@@ -158,6 +189,8 @@ CREATE TABLE `students` (
 
 INSERT INTO `students` (`Student_name`, `Student_USN`, `Student_password`, `Student_phoneNo`, `Student_email`) VALUES
 ('Admin1', '1', 'abc', 12345, 'admin1@gmail.com'),
+('ABC', '10', 'abc', 5768798, 'abc@gmail.com'),
+('abc', '11', 'xyz', 464565, 'xyz@gmail.com'),
 ('Admin2', '2', 'abc', 23411, 'admin2@gmail.com'),
 ('Admin3', '3', 'abc', 56789, 'admin3@gmail.com'),
 ('Student1', '4', 'abc', 234568, 'student1@gmail.com'),
@@ -184,6 +217,19 @@ ALTER TABLE `club`
   ADD PRIMARY KEY (`Club_ID`);
 
 --
+-- Indexes for table `clubjoinform`
+--
+ALTER TABLE `clubjoinform`
+  ADD PRIMARY KEY (`Club_ID`);
+
+--
+-- Indexes for table `clubregistration`
+--
+ALTER TABLE `clubregistration`
+  ADD PRIMARY KEY (`Club_ID`,`Member_USN`),
+  ADD KEY `Member_USN` (`Member_USN`);
+
+--
 -- Indexes for table `club_member`
 --
 ALTER TABLE `club_member`
@@ -194,37 +240,19 @@ ALTER TABLE `club_member`
 -- Indexes for table `create_event`
 --
 ALTER TABLE `create_event`
-  ADD PRIMARY KEY (`event_id`),
-  ADD KEY `club_id` (`club_id`);
+  ADD KEY `create_event_ibfk_1` (`club_id`);
 
 --
 -- Indexes for table `past_event`
 --
 ALTER TABLE `past_event`
-  ADD PRIMARY KEY (`event_id`),
-  ADD KEY `club_id` (`club_id`) USING BTREE;
+  ADD KEY `past_event_ibfk_1` (`club_id`);
 
 --
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`Student_USN`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `create_event`
---
-ALTER TABLE `create_event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- AUTO_INCREMENT for table `past_event`
---
-ALTER TABLE `past_event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
@@ -236,6 +264,19 @@ ALTER TABLE `past_event`
 ALTER TABLE `admin`
   ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`Admin_USN`) REFERENCES `students` (`Student_USN`) ON DELETE CASCADE,
   ADD CONSTRAINT `admin_ibfk_2` FOREIGN KEY (`Club_ID`) REFERENCES `club` (`Club_ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `clubjoinform`
+--
+ALTER TABLE `clubjoinform`
+  ADD CONSTRAINT `clubjoinform_ibfk_1` FOREIGN KEY (`Club_ID`) REFERENCES `club` (`Club_ID`);
+
+--
+-- Constraints for table `clubregistration`
+--
+ALTER TABLE `clubregistration`
+  ADD CONSTRAINT `clubregistration_ibfk_1` FOREIGN KEY (`Club_ID`) REFERENCES `club` (`Club_ID`),
+  ADD CONSTRAINT `clubregistration_ibfk_2` FOREIGN KEY (`Member_USN`) REFERENCES `students` (`Student_USN`);
 
 --
 -- Constraints for table `club_member`
@@ -255,7 +296,6 @@ ALTER TABLE `create_event`
 --
 ALTER TABLE `past_event`
   ADD CONSTRAINT `past_event_ibfk_1` FOREIGN KEY (`club_id`) REFERENCES `club` (`Club_ID`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
