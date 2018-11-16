122c155284f25f4af116529762b007c435b629fd
Tolo5star <tolo5star@gmail.com>
2018-11-17 00:35:32 +0530

    Update query.php

--- RoomBlocking/query.php
+++ RoomBlocking/query.php
@@ -9,28 +9,27 @@
 		var n=ne.innerHTML;
 		window.location.href="ins.php?id="+r+"&fr="+f+"&td="+t+"&n="+n;
 	}
+	function Ret(){
+		window.location.href="page.html";
+	}
 </script>
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "portal";
-
+$dbname = "room_blocking";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 $name=$_POST["Name"];
 $Frd=$_POST["Dp"];
 $Frd = date('Y-m-d', strtotime(str_replace('-', '/', $Frd)));
 $Td=$_POST["Dp1"];
 $Td = date('Y-m-d', strtotime(str_replace('-', '/', $Td)));
 $En=$_POST["Ename"];
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 $sql ="SELECT RoomId FROM rooms r WHERE NOT EXISTS (SELECT * FROM bookings b WHERE r.roomid = b.roomid AND (".'\''.$Frd.'\''." BETWEEN b.from_d AND b.to_d OR ".'\''.$Td.'\''." BETWEEN b.from_d AND b.to_d))";
 //echo $sql;
 $result = $conn->query($sql);
@@ -44,8 +43,8 @@ if ($result->num_rows > 0) {
     echo '<div id="Name" style="display : none">'.'\''.$En.'\''.' </div>';
 } 
 else {
-    echo "<script>alert('Invalid Input')</script>";
+    echo "<script>alert('No Results')</script><br>";
+    echo '<button type="button" onclick="Ret()">Return to Booking Page</button><br>';
 }
 $conn->close();
-
 ?>
