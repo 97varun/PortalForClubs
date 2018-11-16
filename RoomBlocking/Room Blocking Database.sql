d4bf50980887e69be3e6f15ec9228183e0ad04ec
Tolo5star <tolo5star@gmail.com>
2018-11-16 23:30:30 +0530

    Update Room Blocking Database.sql
    Added some statements to populate the database

--- RoomBlocking/Room Blocking Database.sql
+++ RoomBlocking/Room Blocking Database.sql
@@ -12,3 +12,7 @@ CREATE TABLE `bookings` (
  KEY `RoomId` (`RoomId`),
  CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`RoomId`) REFERENCES `rooms` (`RoomId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `rooms` (`RoomId`, `RoomName`) VALUES ('1', 'Auditorium'), ('2', 'Room2'), ('3', 'Room3'), ('4', 'Room4'), ('5', 'Room5');
+
+INSERT INTO `bookings` (`RoomId`, `From_D`, `To_d`, `B_Name`) VALUES ('1', '2018-11-01', '2018-11-03', 'Trance'), ('2', '2018-11-02', '2018-11-07', 'Alcoding'), ('3', '2018-11-01', '2018-11-04', 'DebSoc'), ('4', '2018-11-12', '2018-11-17', 'MunSoc'), ('5', '2018-11-03', '2018-11-06', 'Pulse');
