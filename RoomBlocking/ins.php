40287e7ff2ad1c39e157fa89254be046153edf0e
Tolo5star <tolo5star@gmail.com>
2018-11-17 00:34:48 +0530

    Update ins.php

--- RoomBlocking/ins.php
+++ RoomBlocking/ins.php
@@ -1,25 +1,27 @@
+<script type="text/javascript">
+	function Ret(){
+		window.location.href="page.html";
+	}
+</script>
+
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "portal";
-
-
+$dbname = "room_blocking";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 $sql = "INSERT INTO bookings
 VALUES(".$_GET["id"].",".$_GET["fr"].",".$_GET["td"].",".$_GET["n"].")";
-
 if ($conn->query($sql) === TRUE) {
-    echo "Booking successful";
+    echo "Booking successful<br>";
+    echo '<button type="button" onclick="Ret()">Return to Booking Page</button><br>';
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
 $conn->close();
 ?>
