bf62e61ccc8887d364cfe084f729d26d59cf06ed
Swethaksn <43950407+Swethaksn@users.noreply.github.com>
2018-11-18 01:06:59 +0530

    Update admin.php

--- admin/php/admin.php
+++ admin/php/admin.php
@@ -2,19 +2,32 @@
    
 
 	session_start(); 
+
     $mysqli = new mysqli("localhost", "root", "", "portal");
+
 	//$_SESSION['srn']=1;
     $result = $mysqli->query("SELECT Student_name FROM students WHERE Student_USN = '" . $_SESSION['srn'] . "'");
     $row = $result->fetch_assoc();
-	
-	$result = $mysqli->query("SELECT Club_ID FROM admin WHERE Admin_USN = '" . $_SESSION['srn'] . "'");
-    $x = $result->fetch_assoc();
-	
-	$result = $mysqli->query("SELECT Club_name FROM club WHERE Club_ID = '" . $x['Club_ID'] . "'");
-    $r = $result->fetch_assoc();
-	
-    $string = $row['Student_name'].";".$r['Club_name'];
-    echo($string);
+    $string = $row['Student_name'].";".$_SESSION['srn'];
+    
+
+    $link=mysqli_connect("localhost","root","","portal");
+	extract($_SESSION);
+    $srn = $_SESSION['srn'];
+
+    $sql="SELECT Club_ID FROM admin where Admin_USN='$srn';";
+	$ret=mysqli_query($link,$sql);
+	$row=mysqli_fetch_row($ret);
+	$clubID=$row[0];
+
+
+	$sql="SELECT Club_name FROM club where Club_ID='$clubID';";
+	$ret=mysqli_query($link,$sql);
+	$row=mysqli_fetch_row($ret);
+	$clubname=$row[0];
+	$_SESSION['clubname']=$clubname;
+
+echo($string);
     // close connection
     mysqli_close($mysqli);
   ?>
