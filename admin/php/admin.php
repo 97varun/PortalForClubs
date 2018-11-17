e267833008f43685b77fcc7bd80913218c1de971
Swethaksn <43950407+Swethaksn@users.noreply.github.com>
2018-11-18 01:09:41 +0530

    Update admin.php

--- admin/php/admin.php
+++ admin/php/admin.php
@@ -8,7 +8,14 @@
 	//$_SESSION['srn']=1;
     $result = $mysqli->query("SELECT Student_name FROM students WHERE Student_USN = '" . $_SESSION['srn'] . "'");
     $row = $result->fetch_assoc();
-    $string = $row['Student_name'].";".$_SESSION['srn'];
+    	
+	$result = $mysqli->query("SELECT Club_ID FROM admin WHERE Admin_USN = '" . $_SESSION['srn'] . "'");
+    $x = $result->fetch_assoc();
+	
+	$result = $mysqli->query("SELECT Club_name FROM club WHERE Club_ID = '" . $x['Club_ID'] . "'");
+    $r = $result->fetch_assoc();
+	
+    $string = $row['Student_name'].";".$r['Club_name'];
     
 
     $link=mysqli_connect("localhost","root","","portal");
