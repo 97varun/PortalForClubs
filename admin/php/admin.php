05dfcc42599211824b7c120a28d17f25ce07d250
Swethaksn <43950407+Swethaksn@users.noreply.github.com>
2018-11-17 14:03:28 +0530

    Update admin.php
    added code to fetch club name

--- admin/php/admin.php
+++ admin/php/admin.php
@@ -6,7 +6,14 @@
 	//$_SESSION['srn']=1;
     $result = $mysqli->query("SELECT Student_name FROM students WHERE Student_USN = '" . $_SESSION['srn'] . "'");
     $row = $result->fetch_assoc();
-    $string = $row['Student_name'].";".$_SESSION['srn'];
+	
+	$result = $mysqli->query("SELECT Club_ID FROM admin WHERE Admin_USN = '" . $_SESSION['srn'] . "'");
+    $x = $result->fetch_assoc();
+	
+	$result = $mysqli->query("SELECT Club_name FROM club WHERE Club_ID = '" . $x['Club_ID'] . "'");
+    $r = $result->fetch_assoc();
+	
+    $string = $row['Student_name'].";".$r['Club_name'];
     echo($string);
     // close connection
     mysqli_close($mysqli);
