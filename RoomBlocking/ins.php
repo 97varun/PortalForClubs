f8891629b3ecb4a760423da372e80a4786533c22
Tolo5star <tolo5star@gmail.com>
2018-11-17 21:32:33 +0530

    Update ins.php
    css added

--- RoomBlocking/ins.php
+++ RoomBlocking/ins.php
@@ -8,7 +8,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "portal";
+$dbname = "room_blocking";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -18,8 +18,11 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO bookings
 VALUES(".$_GET["id"].",".$_GET["fr"].",".$_GET["td"].",".$_GET["n"].")";
 if ($conn->query($sql) === TRUE) {
-    echo "Booking successful<br>";
-    echo '<button type="button" onclick="Ret()">Return to Booking Page</button><br>';
+	echo '<link rel="stylesheet" href="main.css">';
+    echo '<body class="login">';
+    echo "<h1>Booking successful</h1><br>";
+    echo '<button type="button" onclick="Ret()" class="btn btn-primary btn-block btn-large">Return to Booking Page</button><br>';
+    echo "</body>";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
