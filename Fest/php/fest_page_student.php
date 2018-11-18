e7ec29ed49c4eb3be97384ab7307417dd8599a7f
KavyaThani <30548697+KavyaThani@users.noreply.github.com>
2018-11-18 19:56:45 +0530

    Update fest_page_student.php

--- Fest/php/fest_page_student.php
+++ Fest/php/fest_page_student.php
@@ -5,7 +5,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" href="../css/main2.css" />
+		<link rel="stylesheet" href="../../notifications/css/main2.css" />
 		<!--Menu Bar-->
 		<link rel="stylesheet" href="../../admin/css/navigation.css" >
 		<script  src="../../admin/js/navigation.js"></script> 
