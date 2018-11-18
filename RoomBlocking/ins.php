f1362c4dd10aba92f76b9291604065222694f028
Tolo5star <tolo5star@gmail.com>
2018-11-18 09:44:07 +0530

    Update ins.php
    navigation menu

--- RoomBlocking/ins.php
+++ RoomBlocking/ins.php
@@ -19,6 +19,19 @@ $sql = "INSERT INTO bookings
 VALUES(".$_GET["id"].",".$_GET["fr"].",".$_GET["td"].",".$_GET["n"].")";
 if ($conn->query($sql) === TRUE) {
 	echo '<link rel="stylesheet" href="main.css">';
+    echo '<link rel="stylesheet" href="navigation.css">';
+        echo '<script  src="navigation.js"></script>';
+        echo '<div class="container" onclick="myFunction(this)">
+              <div class="bar1"></div>
+              <div class="bar2"></div>
+              <div class="bar3"></div>
+            </div>
+            <div id="myDropdown" style="visibility:hidden;" class="dropdown-content" >
+                <a href="../../student/html/student.html">Home</a></br>
+                <a href="../../EventCalendar/main.html">Calendar</a></br>
+                <a href="../php/logout.php">Logout</a>
+             
+            </div>  ';
     echo '<body class="login">';
     echo "<h1>Booking successful</h1><br>";
     echo '<button type="button" onclick="Ret()" class="btn btn-primary btn-block btn-large">Return to Booking Page</button><br>';
