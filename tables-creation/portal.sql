f178efa3b4546ebd6876c572ed434b23ce3c45db
Tolo5star <tolo5star@gmail.com>
2018-11-18 22:17:54 +0530

    Update portal.sql
    Increased the size of B_Name in bookings

--- tables-creation/portal.sql
+++ tables-creation/portal.sql
@@ -659,7 +659,7 @@ CREATE TABLE `bookings` (
  `RoomId` int(4) NOT NULL,
  `From_D` date NOT NULL,
  `To_d` date NOT NULL,
- `B_Name` varchar(20) NOT NULL,
+ `B_Name` varchar(30) NOT NULL,
  KEY `RoomId` (`RoomId`),
  CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`RoomId`) REFERENCES `rooms` (`RoomId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
